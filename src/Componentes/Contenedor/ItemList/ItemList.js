152e001de9615c2f9738b7b32239ee799d349b84
salo2019 <jorgesalo86@gmail.com>
2022-10-10 04:17:08 -0300

    agregando funcionalidad y API

--- src/Componentes/Contenedor/ItemList/ItemList.js
+++ src/Componentes/Contenedor/ItemList/ItemList.js
@@ -1,13 +1,31 @@
 import React from "react";
 
-export const ItemList = ( {productos} ) => {
-    return (
-    <>
-        {
-            productos.map((producto) => {
-                return <h2 key={producto.id}>{producto.nombre}</h2>
-            })
-        }
+//bootstrap
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+
+export const ItemList = ({ productos }) => {
+    return <>
+        <CardGroup>
+            {
+                productos.map((producto) => {
+                    return (
+                        <Card key={producto.id} className="p-2">
+                            <Card.Img variant="top" width="110" height="180" src={producto.image} />
+                            <Card.Body>
+                                <Card.Title>${producto.price}</Card.Title>
+                                <Card.Text>
+                                    {producto.title}
+                                </Card.Text>
+                            </Card.Body>
+                            <Card.Footer>
+                                <Button variant="primary">Mas detalles</Button>
+                            </Card.Footer>
+                        </Card>
+                    )
+                })
+            }
+        </CardGroup>
     </>
-    )
 }
--- src/Componentes/Contenedor/ItemListContainer.js
+++ src/Componentes/Contenedor/ItemListContainer.js
@@ -4,34 +4,17 @@ import Card from 'react-bootstrap/Card';
 import { ItemList } from "./ItemList/ItemList";
 //import { FuncionComponenteEstados } from "../ItemCount/ItemCount";
 
-
-const productos = [
-  {nombre:"manzana", id:0, categoria:"fruta", stock:10, precio:500},
-  {nombre:"pera", id:1, categoria:"fruta", stock:15, precio:700},
-  {nombre:"lechuga", id:2, categoria:"verdura", stock:12, precio:200},
-  {nombre:"tomate", id:3, categoria:"verdura", stock:19, precio:850},
-  {nombre:"banana", id:4, categoria:"fruta", stock:14, precio:1200}
-]
-
-//creacion de promesa. No se deberian crear aqui.
-const obtenerProductos = new Promise((resolve, reject) => {
-  setTimeout(()=>{
-    resolve(productos);
-  },2000);
-})
-
 export const ItemListContainer = ( { greeting } ) => {
 
   const [productos, setProductos] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(()=>{
-    obtenerProductos
-    .then((data)=>{
-      setProductos(data);
-    })
+    fetch("https://fakestoreapi.com/products/category/electronics")
+    .then((res)=> res.json())
+    .then((datos)=> setProductos(datos))
     .catch((e)=>{
-      console.log("salio todo mal");
+      console.log("error de consulta");
       console.log(e);
     })
     .finally(()=>{
